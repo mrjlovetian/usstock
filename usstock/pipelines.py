6a5e5d79492e10cb0486d6d3ebab9643bd0f6dae
caiwenming <caiwenming@myhexin.com>
2019-04-15 11:01:32 +0800

    add file

--- usstock/pipelines.py
+++ usstock/pipelines.py
@@ -5,7 +5,14 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
+import pymysql
 
 class UsstockPipeline(object):
     def process_item(self, item, spider):
+        db = pymysql.connect('localhost', 'root', '897011805', 'yhj')
+        cursor = db.cursor()
+        sql = """INSERT INTO usstock values ('%s', '%s', '%s')"""%(item['name'], item['symbol'], item['no'])
+        cursor.execute(sql)
+        db.commit()
+        db.close()
         return item
--- usstock/spiders/us.py
+++ usstock/spiders/us.py
@@ -1,11 +1,21 @@
 # -*- coding: utf-8 -*-
 import scrapy
-
+from usstock.items import UsstockItem
+import json
 
 class UsSpider(scrapy.Spider):
     name = 'us'
     allowed_domains = ['163.com']
-    start_urls = ['http://163.com/']
+    baseUrl = 'http://quotes.money.163.com/us/service/usrank.php?host=/us/service/usrank.php&page=0&query=UPDATE:_exists_true;PRICE:_exists_true;typename:nasdaq&fields=no,SYMBOL,NAME,PRICE,UPDOWN,PERCENT,WEEK52_HIGH,WEEK52_LOW,TCAP,PE&count=2500&type=query&callback=callback_704314870&req=11010'
+    start_urls = [baseUrl]
 
     def parse(self, response):
-        pass
+        usstocks = json.loads(response.body)['list']
+        for usstock in usstocks:
+            item = UsstockItem()
+            item['name'] = usstock['NAME']
+            item['symbol'] = usstock['SYMBOL']
+            item['no'] = usstock['NO']
+            yield item
+        
+        

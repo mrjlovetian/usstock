a5fe8f810921c43032b23b10cf15c7c2744bdf74
caiwenming <caiwenming@myhexin.com>
2019-04-15 11:26:34 +0800

    add file

--- usstock/spiders/us.py
+++ usstock/spiders/us.py
@@ -6,18 +6,18 @@ import json
 class UsSpider(scrapy.Spider):
     name = 'us'
     allowed_domains = ['163.com']
-    baseUrl = 'http://quotes.money.163.com/us/service/usrank.php?host=/us/service/usrank.php&page=0&query=UPDATE:_exists_true;PRICE:_exists_true;typename:nasdaq&fields=no,SYMBOL,NAME,PRICE,UPDOWN,PERCENT,WEEK52_HIGH,WEEK52_LOW,TCAP,PE&count=2&type=query&callback=callback_704314870&req=11010'
+    baseUrl = 'http://quotes.money.163.com/us/service/usrank.php?host=/us/service/usrank.php&page=0&query=UPDATE:_exists_true;PRICE:_exists_true;typename:nasdaq&fields=no,SYMBOL,NAME,PRICE,UPDOWN,PERCENT,WEEK52_HIGH,WEEK52_LOW,TCAP,PE&count=2&type=query&req=11010'
     start_urls = [baseUrl]
 
     def parse(self, response):
-        body = json.loads(response.body)
-        print ('........................', body)
-        # usstocks = json.loads(response.body)['list']
-        # for usstock in usstocks:
-        #     item = UsstockItem()
-        #     item['name'] = usstock['NAME']
-        #     item['symbol'] = usstock['SYMBOL']
-        #     item['no'] = usstock['NO']
-        #     yield item
+        # body = json.loads(response.body)
+        # print ('........................', body)
+        usstocks = json.loads(response.body)['list']
+        for usstock in usstocks:
+            item = UsstockItem()
+            item['name'] = usstock['NAME']
+            item['symbol'] = usstock['SYMBOL']
+            item['no'] = usstock['NO']
+            yield item
         
         
